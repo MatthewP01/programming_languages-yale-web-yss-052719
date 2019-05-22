33f5e486d0ed5741fce10e3f93f3901fd1d850eb
stupendous-blog-8362 <matthew.peoples@yale.edu>
2019-05-22 21:01:19 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -30,32 +30,32 @@ languages = {
   }
 }
 
-def reformat_languages(languages)
-  new_hash = {}
-  languages.each do |style, language|
-    language.each do |name, attributes|
-    new_languages[name] ||= attributes
-    new_languages[name][:style]=[]
-    new_languages[name][:style]<<style
-    end
-  end
-  new_hash
-end
-
 # def reformat_languages(languages)
-#   new_languages = {}
+#   new_hash = {}
 #   languages.each do |style, language|
-#     language.each do |language_name, attribute|
-#       if new_languages[language_name] == nil
-#           new_languages[language_name] = attribute
-#         end
-#       new_languages[language_name][:style] = []
-#       new_languages[language_name][:style] << style
+#     language.each do |name, attributes|
+#     new_languages[name] ||= attributes
+#     new_languages[name][:style]=[]
+#     new_languages[name][:style]<<style
 #     end
 #   end
-#   new_languages[:javascript][:style] << :oo
-#   new_languages
+#   new_hash
 # end
 
+def reformat_languages(languages)
+  new_languages = {}
+  languages.each do |style, language|
+    language.each do |language_name, attribute|
+      if new_languages[language_name] == nil
+          new_languages[language_name] = attribute
+        end
+      new_languages[language_name][:style] = []
+      new_languages[language_name][:style] << style
+    end
+  end
+  new_languages[:javascript][:style] << :oo
+  new_languages
+end
 
-reformat_languages(languages)
+
+# reformat_languages(languages)
